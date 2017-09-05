543a1ab517db8e08698620a3971616b0f0ffffb8
Ulfasaar <weyers.ryan@gmail.com>
2017-09-05 12:04:37 +1200

    Code compiles but isn't tested so do not use this code the wifi code might be buggy

--- Main/WebClientRepeating.ino
+++ Main/WebClientRepeating.ino
@@ -91,88 +91,88 @@ void WIFI_send()
 
 
 
-// use other code for own at commands etc to see how to send real values
+// // use other code for own at commands etc to see how to send real values
 
-// void reboot_connection()
-
-
-int port = 80;
-char protocol[5] = "\0";
-
-void store_config(){
-  if(SD_available){
-    myFile = SD.open(F("config.txt"), FILE_WRITE);
-
-    // store data in buffer
-    sprintf_P(Buffer, PSTR("PORT %d\nSERVER %s\nPROTOCOL %s"), port,  Buffer, protocol);
-
-    // write the variables into the config file
-    Save(Buffer);
-  }
-}
+// // void reboot_connection()
 
 
-void update_config(){
-  // connect to massey
-  strncpy_P(Buffer, PSTR("seat-skomobo.massey.ac.nz"), 26);
+// // int port = 80;
+// // char protocol[5] = "\0";
 
-  if (client.connect(Buffer, 80)) {
+// void store_config(){
+//   if(SD_available){
+//     myFile = SD.open(F("config.txt"), FILE_WRITE);
 
-    strncpy_P(Buffer, PSTR("GET /config HTTP/1.1\n"), 23);
-    client.print(Buffer);
-    strncpy_P(Buffer, PSTR("Host: seat-skomobo.massey.ac.nz\r\n"), 34);
-    client.print(Buffer);
-    strncpy_P(Buffer, PSTR("Connection: close\r\n\r\n"), 22);
-    client.print(Buffer);
+//     // store data in buffer
+//     sprintf_P(Buffer, PSTR("PORT %d\nSERVER %s\nPROTOCOL %s"), port,  Buffer, protocol);
 
-    // debugging for now
-    Serial.println(client.read());
+//     // write the variables into the config file
+//     Save(Buffer);
+//   }
+// }
 
-    show_P("Updating config");
 
-    // variables get reassigned here
+// void update_config(){
+//   // connect to massey
+//   strncpy_P(Buffer, PSTR("seat-skomobo.massey.ac.nz"), 26);
 
-    // server gets stored in buffer
+//   if (client.connect(Buffer, 80)) {
 
-    // if there is no reply we check the config file
+//     strncpy_P(Buffer, PSTR("GET /config HTTP/1.1\n"), 23);
+//     client.print(Buffer);
+//     strncpy_P(Buffer, PSTR("Host: seat-skomobo.massey.ac.nz\r\n"), 34);
+//     client.print(Buffer);
+//     strncpy_P(Buffer, PSTR("Connection: close\r\n\r\n"), 22);
+//     client.print(Buffer);
+
+//     // debugging for now
+//     Serial.println(client.read());
+
+//     show_P("Updating config");
+
+//     // variables get reassigned here
+
+//     // server gets stored in buffer
+
+//     // if there is no reply we check the config file
     
-    //if(reply){
-    store_config();
-    //}
-    //else{
-    // read_config();
-    // if(data_in_config){
-      // assign config values
-    //}
-    // else{
-//      reassign default values
-    //}
-    //}
+//     //if(reply){
+//     store_config();
+//     //}
+//     //else{
+//     // read_config();
+//     // if(data_in_config){
+//       // assign config values
+//     //}
+//     // else{
+// //      reassign default values
+//     //}
+//     //}
 
 
-  }else{
-    show_P("No update");
-  }
+//   }else{
+//     show_P("No update");
+//   }
 
 
-}
+// }
 
 
-void tcp_send_data(){
-  // if there's a successful connection
+// void tcp_send_data(){
+//   // if there's a successful connection
 
- show_P("Sending data\nto server");
+//  show_P("Sending data\nto server");
  
- // First variable is the tcp 'route'
+//  // First variable is the tcp 'route'
 
- snprintf_P(Buffer, 70, PSTR("0_" BOX_ID "_%d-%d-%d-%d-%d-%d_%d_%d_%d_%d.%d_%d.%d_%d_%c\n"),  year, month, day, hour, minute, second, PM1, PM25, PM10,(int)temperature, (int)(temperature * 100) % 100, (int)humidity, (int)(humidity * 100) % 100, CO2, PIR);
- client.print(Buffer);
+//  snprintf_P(Buffer, 70, PSTR("0_" BOX_ID "_%d-%d-%d-%d-%d-%d_%d_%d_%d_%d.%d_%d.%d_%d_%c\n"),  year, month, day, hour, minute, second, PM1, PM25, PM10,(int)temperature, (int)(temperature * 100) % 100, (int)humidity, (int)(humidity * 100) % 100, CO2, PIR);
+//  client.print(Buffer);
 
- show_P("Data sent");
- // note the time that the connection was made
- lastConnectionTime = millis();
+//  show_P("Data sent");
+//  // note the time that the connection was made
+//  lastConnectionTime = millis();
 
-}
+// }
 
 void http_send_data(){
  
@@ -201,28 +201,32 @@ void httpRequest()
   // move this to bottom for my own protocol
   client.stop();
 
-  show_P("Updating config");
-  update_config();
+  // show_P("Updating config");
+  // update_config();
 
   show_P("Connecting\n to server");
 
-  // strncpy_P(Buffer, PSTR("seat-skomobo.massey.ac.nz"), 26);
+  strncpy_P(Buffer, PSTR("seat-skomobo.massey.ac.nz"), 26);
     
   //update config puts domain received from massey in buffer
-  if (client.connect(Buffer, port)) {
+  // if (client.connect(Buffer, port)) {
 
-    if(strcmp_P(protocol,PSTR("TCP"))){
-        tcp_send_data();
-    }
-    else if(strcmp_P(protocol,PSTR("blue"))){
-        blue_send();
-    }
-    else{
+  if (client.connect(Buffer, 80)) {
+    
+    http_send_data();
+    
+    // if(strcmp_P(protocol,PSTR("TCP"))){
+    //     tcp_send_data();
+    // }
+    // else if(strcmp_P(protocol,PSTR("blue"))){
+    //     blue_send();
+    // }
+    // else{
 
-        // here if it is HTTP or an invalid protocol we just assume to use HTTP, better than
-        // it not sending anything because the protocol is wrong :)
-        http_send_data();
-    }
+    //     // here if it is HTTP or an invalid protocol we just assume to use HTTP, better than
+    //     // it not sending anything because the protocol is wrong :)
+    //     http_send_data();
+    // }
 
   }else{
     show_P("Server\nconnection\nlost");
